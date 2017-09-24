142dae3fe52c6d7331b485c94f592feacfb945b7
mango <mango@mangos-MacBook-Pro.local>
2017-09-24 00:12:22 -0500

    Added 3 functions into single data.sol

--- contracts/data.sol
+++ contracts/data.sol
@@ -1,23 +1,76 @@
 pragma solidity ^0.4.4;
 
-contract data {
+
+/*******************************************************************************************    
+        Mapper Contract - holds mapping & structs for market participants
+*******************************************************************************************/
+
+contract mapper {
+
+    /************************************** 
+        Structs and Variables
+    **************************************/
+
+    //holds demographic data for each user
+    struct demographicsData {
+        uint race;
+        uint16 location;
+        bool gender; 
+        uint dob;   //year of birth
+    }
+
+    //holds researcher
+    struct researcher {
+        uint institution;
+    }
+
+    //holds buyer
+    struct buyer {
+        uint institution;
+    }
+
+    //participants
+    mapping(address => demographicsData) public participants;
+
+    //researchers
+    mapping (address => researcher) public researchers;
+
+    /************************************** 
+        Functions
+    **************************************/
+
+    //add participants to the participants mapping
+    function addParticipants(address _addy, uint _dob, bool _gender, uint _location, uint _race) public {
+        participants[_addy] = demographicsData(_dob, _gender, _location, _race);
+    }
+
+    //add researcher to the researcher mapping
+    function addResearcher(address _addy, uint _institution) public {
+        researchers[_addy] = researcher(_institution);
+    }
+
+    //add buyer to the buyer mapping
+    function addBuyer(address _addy, uint _institution) public {
+        researchers[_addy] = researcher(_institution);
+    }
+
+}
+
+/*******************************************************************************************    
+        Data Contract - maintains object holding medical research data
+*******************************************************************************************/
+
+contract data is mapper {
 
 
     /************************************** 
         Structs and Variables
     **************************************/
 
-    mapping (uint => dataPoint) public coreData;
+    mapping (uint => mapping(uint => dataPoint)) public coreData;
     mapping (address => user) userData;
 
-    uint coreDataIter; //iterator pointer for coreData
-
-    struct user {
-        uint16 location;
-        bool gender; 
-        uint dob;   //year of birth
-        uint8 race;
-    }
+    uint dataPointIter = 1; //iterator pointer for dataPoint     
 
     struct dataPoint {
         uint id;
@@ -32,21 +85,41 @@ contract data {
         Functions
     **************************************/
 
-    /* Researcher submits data */
-    function submitData(address _address,bytes32 _meta, uint256 _price, uint _data) returns(bool success) {
+    // Researcher submits data 
+    function submitData(uint _id, address _address,bytes32 _meta, uint256 _price, uint _data) returns(bool success) {
         dataPoint memory newDataPoint;
-		newDataPoint.id = 1333;
+		newDataPoint.id = dataPointIter;
 		newDataPoint.owner = msg.sender;
 		newDataPoint.participant = _address;
         newDataPoint.meta = _meta;
         newDataPoint.price = _price;
         newDataPoint.data = _data;
 
-        coreData[coreDataIter] = newDataPoint; //changes state
+        coreData[_id][dataPointIter] = newDataPoint; //changes state
+        dataPointIter += 1; //update pointe
 
         //add event
 		return true;	
     }
+
+    //return data for typeID
+    function query (uint typeID) constant returns (uint[] prices_,bytes32[] meta_, uint[] id_) {
+        uint length = dataPointIter;
+        uint[] memory prices = new uint[](length);
+        bytes32[] memory meta = new bytes32[](length);
+        uint[] memory id = new uint[](length);
+
+        for (uint i = 0; i<length; i++) {
+            if(coreData[typeID][i].id > 0) {
+                prices[i] = coreData[typeID][i].price;
+                meta[i] = coreData[typeID][i].meta;
+                id[i] = coreData[typeID][i].id;
+            }
+        }
+
+        return(prices,meta,id);
+    }
+
 
     /************************************** 
         Modifiers
@@ -61,6 +134,10 @@ contract data {
 
 }
 
+/*******************************************************************************************    
+        Marketplace Contract - Define buy functionality
+*******************************************************************************************/
+
 contract marketplace is data {
 
 
@@ -68,28 +145,11 @@ contract marketplace is data {
         Functions
     **************************************/
 
-    function buy (uint _id, uint val) returns (uint id_,bytes32 meta_, uint price_, uint data_) {
-        //conditional/require
-        require(coreData[_id]);
-        coreData[_id].owner.transfer(val);
+    function buy (uint typeID, uint _id, uint val) returns (uint id_,bytes32 meta_, uint price_, uint data_) {
+        require(coreData[typeID][_id].id>0);
+        coreData[typeID][_id].owner.transfer(val);
 
-        return(coreData[_id].id,coreData[_id].meta,coreData[_id].price,coreData[_id].data);
-    }
-
-    function query (uint _id) constant returns (uint[] prices_,bytes32[] meta_, uint[] id_) {
-        uint length = coreData[_id].length;
-        uint[] memory prices = new uint[](length);
-        bytes32[] memory meta = new bytes32[](length);
-        uint[] memory id = new uint[](length);
-
-        for (uint i = 0; i<length; i++) {
-            //dataPoint memory cur; //pointer to dataPoint
-            prices[i] = coreData[_id].price;
-            meta[i] = coreData[_id].meta;
-            id[i] = coreData[_id].id;
-        }
-
-        return(prices,meta,id);
+        return(coreData[typeID][_id].id,coreData[typeID][_id].meta,coreData[typeID][_id].price,coreData[typeID][_id].data);
     }
 
 }
