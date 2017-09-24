8ad41db1bea24226400ae345e1d4a07cf2aa6056
aokoli <okolialex@gmail.com>
2017-09-23 22:15:29 -0500

    Added axios for API calls

--- src/scenes/SourceResearcher/index.js
+++ src/scenes/SourceResearcher/index.js
@@ -1,23 +1,39 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form'
 import { connect } from 'react-redux'
+import axios from 'axios'
 import {
   TextField
 } from 'redux-form-material-ui'
+import RaisedButton from 'material-ui/RaisedButton';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import './styles.css'
 
 
-let SourceResearcher = () => (
+let onSubmit = (values) => {
+  // console.log('[DEBUG] values', values)
+  let url = "https://jsonplaceholder.typicode.com/posts" // TODO: Put localhost link to Ethereum blockchain
+
+  axios.post(url, values)
+  .then((res) => {
+    console.log('[DEBUG] res', res)
+  })
+  .catch((err) => {
+    console.log('[DEBUG] err', err)
+  })
+}
+
+let SourceResearcher = ({handleSubmit}) => (
   <MuiThemeProvider>
     <div className='sr-container'>
-      <div className='sr-content'>
+      <form className='sr-form' onSubmit={handleSubmit(onSubmit)}>
         <Field name='user' floatingLabelText='User Address' component={TextField} fullWidth />
         <Field name='dataId' floatingLabelText='Data ID' component={TextField} fullWidth />
         <Field name='price' floatingLabelText='Price' component={TextField} fullWidth />
-        <Field name='metadata' floatingLabelText='Metadata' component={TextField} fullWidth />
         <Field name='dataValue' floatingLabelText='Data Value' component={TextField} fullWidth />
-      </div>
+        <Field name='metadata' floatingLabelText='Metadata' component={TextField} rows={1} multiLine={true} fullWidth />
+        <RaisedButton label="Submit" type="submit" primary={true} fullWidth className='sr-submit-btn'/>
+      </form>
     </div>
   </MuiThemeProvider>
 )
